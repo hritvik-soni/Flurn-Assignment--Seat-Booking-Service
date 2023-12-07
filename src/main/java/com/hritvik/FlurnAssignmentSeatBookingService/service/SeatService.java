52acf2bcf8d5d5921e836e269d330e12bb384b7b
Hritvik Soni <hritviksoni768@gmail.com>
2023-12-07 11:48:53 +0530

    add

--- src/main/java/com/hritvik/FlurnAssignmentSeatBookingService/service/SeatService.java
+++ src/main/java/com/hritvik/FlurnAssignmentSeatBookingService/service/SeatService.java
@@ -29,48 +29,48 @@ public class SeatService {
         return seatRepository.findById(seatId);
     }
 
-    public boolean isSeatBooked(Long seatId) {
-        Optional<Seat> seat = seatRepository.findById(seatId);
-        return seat.map(Seat::isBooked).orElse(false);
-    }
-
-    public List<Seat> bookSeats(List<Long> seatIds) {
-        List<Seat> bookedSeats = new ArrayList<>();
-
-        for (Long seatId : seatIds) {
-            Optional<Seat> seatOptional = seatRepository.findById(seatId);
-
-            if (seatOptional.isPresent() && !seatOptional.get().isBooked()) {
-                Seat seat = seatOptional.get();
-                seat.setBooked(true);
-                seatRepository.save(seat);
-                bookedSeats.add(seat);
-            }
-        }
-
-        return bookedSeats;
-    }
-
-    public ResponseEntity<SeatDetails> getSeatPricingById(Long id) {
-
-        Optional<Seat> seat = seatRepository.findById(id);
-        SeatDetails seatDetails= new SeatDetails();
-
-        if(seat.isPresent()){
-
-            String seatClass = seat.get().getSeatClass();
-            int totalSeatsInClass = getAllSeats().stream().filter(s -> s.getSeatClass().equals(seatClass)).toArray().length;
-            int bookedSeats = (int) getAllSeats().stream().filter(Seat::isBooked).count();
-            int percentageBooked = (bookedSeats * 100) / totalSeatsInClass;
-            double amount = seatPricingService.calculateBookingAmount(seatClass, percentageBooked);
-
-            seatDetails.setSeatClass(seat.get().getSeatClass());
-            seatDetails.setId(seat.get().getId());
-
-            seatDetails.setSeatPrice(amount);
-            return new ResponseEntity<>(seatDetails,HttpStatus.OK);
-
-        }
-        return new ResponseEntity<>(seatDetails, HttpStatus.BAD_REQUEST);
-    }
+//    public boolean isSeatBooked(Long seatId) {
+//        Optional<Seat> seat = seatRepository.findById(seatId);
+//        return seat.map(Seat::isBooked).orElse(false);
+//    }
+//
+//    public List<Seat> bookSeats(List<Long> seatIds) {
+//        List<Seat> bookedSeats = new ArrayList<>();
+//
+//        for (Long seatId : seatIds) {
+//            Optional<Seat> seatOptional = seatRepository.findById(seatId);
+//
+//            if (seatOptional.isPresent() && !seatOptional.get().isBooked()) {
+//                Seat seat = seatOptional.get();
+//                seat.setBooked(true);
+//                seatRepository.save(seat);
+//                bookedSeats.add(seat);
+//            }
+//        }
+//
+//        return bookedSeats;
+//    }
+//
+//    public ResponseEntity<SeatDetails> getSeatPricingById(Long id) {
+//
+//        Optional<Seat> seat = seatRepository.findById(id);
+//        SeatDetails seatDetails= new SeatDetails();
+//
+//        if(seat.isPresent()){
+//
+//            String seatClass = seat.get().getSeatClass();
+//            int totalSeatsInClass = getAllSeats().stream().filter(s -> s.getSeatClass().equals(seatClass)).toArray().length;
+//            int bookedSeats = (int) getAllSeats().stream().filter(Seat::isBooked).count();
+//            int percentageBooked = (bookedSeats * 100) / totalSeatsInClass;
+//            double amount = seatPricingService.calculateBookingAmount(seatClass, percentageBooked);
+//
+//            seatDetails.setSeatClass(seat.get().getSeatClass());
+//            seatDetails.setId(seat.get().getId());
+//
+//            seatDetails.setSeatPrice(amount);
+//            return new ResponseEntity<>(seatDetails,HttpStatus.OK);
+//
+//        }
+//        return new ResponseEntity<>(seatDetails, HttpStatus.BAD_REQUEST);
+//    }
 }
